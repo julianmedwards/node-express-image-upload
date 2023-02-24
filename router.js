332d34a4ff63d80f44f459a38fa048c27cc48817
julianmedwards <julian.michael.edwards@gmail.com>
2023-02-24 10:29:31 -0600

    Express router.

---
+++ router.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const router = express.Router();
+
+router.get("/", async function (req, res) {
+    await res.render("index");
+});
+module.exports = router;
